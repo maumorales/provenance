ddd0425c4cada9dcd0d199ab9fa7736467cc9d83
maumorales <mau.morales@gmail.com>
2023-11-10 16:51:15 +0000

    Add passing endpoint tests

--- components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -1,13 +1,21 @@
 package io.collective.endpoints;
 
 import io.collective.articles.ArticleDataGateway;
+import io.collective.articles.ArticleInfo;
 import io.collective.restsupport.RestTemplate;
+import io.collective.rss.Item;
+import io.collective.rss.RSS;
 import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
 
 public class EndpointWorker implements Worker<EndpointTask> {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -31,7 +39,11 @@ public class EndpointWorker implements Worker<EndpointTask> {
         gateway.clear();
 
         { // todo - map rss results to an article infos collection and save articles infos to the article gateway
-
+            RSS rss = new XmlMapper().readValue(response, RSS.class);
+            List<Item> items = rss.getChannel().getItem();
+            for (Item item : items) {
+                this.gateway.save(item.getTitle());
+            }
         }
     }
 }
